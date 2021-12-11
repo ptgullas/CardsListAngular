d1f7ef9082305ca44daee0e5f4bff6ec24e18c70
ptgullas <4305689+ptgullas@users.noreply.github.com>
2021-12-11 16:20:04 -0500

    + card-detail & list-tem

---
+++ src/app/shared/card-detail.model.ts
@@ -0,0 +1,7 @@
+import { ListItem } from "./list-item.model";
+
+export class CardDetail {
+    cardId: number = 0;
+    name: string = '';
+    listItems: ListItem[] = [];
+}
---
+++ src/app/shared/list-item.model.ts
@@ -0,0 +1,6 @@
+export class ListItem {
+    listId: number = 0;
+    name: string = '';
+    notes: string = '';
+    sequence: number = 0;
+}
